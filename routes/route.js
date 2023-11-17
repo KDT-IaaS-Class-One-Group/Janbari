728b99053978ecc4517747d9add0fcf95887524d
Yusmin97 <tmdamsi97@gmail.com>
2023-11-17 16:56:07 +0900

    💩 라우트 정적파일 가져오는 문제가 생김 GPT를 통해 엔진 개념 획득

--- routes/route.js
+++ routes/route.js
@@ -2,3 +2,16 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// 정적 파일 서빙 설정
+router.use(express.static(path.join(__dirname, '..', 'public')));
+
+// 뷰 엔진 설정
+// router.set('views', path.join(__dirname, 'views'));
+// router.set('view engine', 'html');
+
+// 라우트 설정
+router.get('/', (req, res) => {
+  res.render('index'); // 'views' vhfejdml 'index.html' 파일을 렌더링
+})
+
+module.exports = router;
