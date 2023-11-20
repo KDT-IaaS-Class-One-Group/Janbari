f72c4c3aa422c8a1d8dc8948eeef345f7e0f0991
Yusmin97 <tmdamsi97@gmail.com>
2023-11-20 15:50:01 +0900

    📝 Modify: json 파일 수정
    json 파일의 get요청을 따로 설정

--- routes/route.js
+++ routes/route.js
@@ -15,7 +15,12 @@ router.use('/public', express.static(path.join(__dirname, '..', 'public')));
 // 라우트 설정
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'views', 'index.html')); // 수정
+})
 
+// json 
+router.get('/json', (req, res) => {
+
+  res.sendFile(path.join(__dirname, '..', 'models', 'info.json'));
   // json 파일 경로
   // join 메서드를 통해 문자열로 바꾸는 작업 진행하였음😀
   const jsonFilePath = path.join(__dirname, '..', 'models', 'info.json');
