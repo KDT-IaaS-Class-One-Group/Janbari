7b300369486777fd53ef81cfa22202f464b18fc5
samuel21695 <samuel21695@gmail.com>
2023-11-20 15:05:18 +0900

    📝 Modify: todo 생성

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('test');
+const profile = document.querySelectorAll('.profile');
+console.log(profile);
+
+// todo json 파일로 이미지 생성하기
+// * 1. eventListener 생성하기
+// * 2. json 파일 데이터 가져오기
+// * 3. 가져온 데이터 값 (백그라운드 이미지) 원하는 클래스에 지정하기
+// todo 이미지 클릭 시 div 생성
+
