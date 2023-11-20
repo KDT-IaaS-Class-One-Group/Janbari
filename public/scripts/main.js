8e0d096d02ffee9647e05e3407654d22a9f33e56
Yusmin97 <tmdamsi97@gmail.com>
2023-11-20 16:11:13 +0900

    📝 Modify: 변수 지정과 이미지 사이즈 조정
    이미지 사이즈를 커버로 하고 id값을 변수로 지정

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,31 @@
-// fetch('/models/info.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     document.getElementById('ho').style.backgroundImage = `url(${data['잔잔바리/호녕'].img})`;
-//     document.getElementById('yu').style.backgroundImage = `url(${data['잔잔바리/승민'].img})`;
-//     document.getElementById('so').style.backgroundImage = `url(${data['잔잔바리/사무엘'].img})`;
-//     document.getElementById('lee').style.backgroundImage = `url(${data['잔잔바리/은정'].img})`;
-//   })
-//   .catch(error => console.error('Error fetching JSON', error));
+const ho = document.getElementById('ho');
+const yu = document.getElementById('yu');
+const so = document.getElementById('so');
+const lee = document.getElementById('lee');
 
 fetch('/json')
-      .then(response => response.json())
-      .then(data => {
-        // Update background images based on JSON data
-        const profiles = document.getElementsByClassName('profile');
-        for (let i = 0; i < profiles.length; i++) {
-          const profileName = profiles[i].getAttribute('data-profile-name');
-          profiles[i].style.backgroundImage = `url(${data[profileName].img})`;
-        }
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
+  .then(response => response.json())
+  .then(data => {
+    ho.style.backgroundImage = `url(${data['잔잔바리/호녕'].img})`;
+    yu.style.backgroundImage = `url(${data['잔잔바리/승민'].img})`;
+    so.style.backgroundImage = `url(${data['잔잔바리/사무엘'].img})`;
+    lee.style.backgroundImage = `url(${data['잔잔바리/은정'].img})`;
+    ho.style.backgroundSize= 'cover';
+    yu.style.backgroundSize= 'cover';
+    so.style.backgroundSize= 'cover';
+    lee.style.backgroundSize= 'cover';
+  })``
+  .catch(error => console.error('Error fetching JSON', error));
+
+// fetch('/json')
+//       .then(response => response.json())
+//       .then(data => {
+//         // Update background images based on JSON data
+//         const profiles = document.getElementsByClassName('profile');
+//         for (let i = 0; i < profiles.length; i++) {
+//           const profileName = profiles[i].getAttribute('data-profile-name');
+//           profiles[i].style.backgroundImage = `url(${data[profileName].img})`;
+//         }
+//       })
+//       .catch(error => console.error('Error fetching JSON:', error));
 
