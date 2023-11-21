ea5f4d49827cda3bc718b31599e4d13cfd36fd9d
Yusmin97 <tmdamsi97@gmail.com>
2023-11-21 11:38:05 +0900

    📝 Modify: 클릭이벤트 생성
    fetch 안에 이벤트리스너를 작성 후
    fetch('/json') 추가 더 간결한 방법이
    있는지 확인해야 될거 같습니다.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,18 +4,49 @@ const so = document.getElementById('so');
 const lee = document.getElementById('lee');
 
 fetch('/json')
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     ho.style.backgroundImage = `url(${data['잔잔바리/호녕'].img})`;
     yu.style.backgroundImage = `url(${data['잔잔바리/승민'].img})`;
     so.style.backgroundImage = `url(${data['잔잔바리/사무엘'].img})`;
     lee.style.backgroundImage = `url(${data['잔잔바리/은정'].img})`;
-    ho.style.backgroundSize= 'cover';
-    yu.style.backgroundSize= 'cover';
-    so.style.backgroundSize= 'cover';
-    lee.style.backgroundSize= 'cover';
-  })``
-  .catch(error => console.error('Error fetching JSON', error));
+    ho.style.backgroundSize = 'cover';
+    yu.style.backgroundSize = 'cover';
+    so.style.backgroundSize = 'cover';
+    lee.style.backgroundSize = 'cover';
+
+    ho.addEventListener('click', () => handleProfileClick('잔잔바리/호녕'));
+    yu.addEventListener('click', () => handleProfileClick('잔잔바리/승민'));
+    so.addEventListener('click', () => handleProfileClick('잔잔바리/사무엘'));
+    lee.addEventListener('click', () => handleProfileClick('잔잔바리/은정'));
+
+    function handleProfileClick(profileId) {
+      // 새로운 div 생성
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('new-profile'); // 예시로 'new-profile' 클래스를 추가
+    
+      // JSON 파일에서 데이터 가져오기
+      fetch('/json')
+        .then(response => response.json())
+        .then(data => {
+          const profileData = data[profileId];
+    
+          // 새로운 div에 데이터 추가
+          newDiv.innerHTML = `
+            <h2>${profileData.name}</h2>
+            <p>${profileData.personal_site}</p>
+            <p>Contact: ${profileData.contact}</p>
+            <p>Current Project: <a href="${profileData.current_project}" target="_blank">${profileData.current_project}</a></p>
+            <img src="${profileData.img}" alt="Profile Image">
+          `;
+    
+          // 새로운 div를 body에 추가
+          document.body.appendChild(newDiv);
+        })
+        .catch(error => console.error('Error fetching JSON', error));
+    }
+  })
+  .catch((error) => console.error('Error fetching JSON', error));
 
 // fetch('/json')
 //       .then(response => response.json())
@@ -28,4 +59,3 @@ fetch('/json')
 //         }
 //       })
 //       .catch(error => console.error('Error fetching JSON:', error));
-
