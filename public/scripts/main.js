29be2cc0bfcba37c6b8796cc3398b6b2b07460b2
Yusmin97 <tmdamsi97@gmail.com>
2023-11-28 14:55:45 +0900

    📝 Modify: 버튼 아이콘 변경
    ❌아이콘으로 변경

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -36,7 +36,7 @@ fetch('/json')
           newDiv.classList.add('new-profile');
           newDiv.innerHTML = `
             <div class = "position-abs left-88vw top-33vh">
-              <button class = "fontSiez-2rem" onclick="handleBack()">🔙</button>
+              <button class = "fontSiez-2rem bgc-white border-none" onclick="handleBack()">❌</button>
             </div>
             <div class = "display-flex flex-column width-42-5vw height-65vh">
               <div class = "display-flex justify-center align-center width-42-5vw height-10vh">
