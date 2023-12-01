cb77c236962aaceb781b04550f9a84f480b85448
Ho Nyeong <dev.honing@gmail.com>
2023-12-01 11:46:07 +0900

    :sparkles: Feat: 테이블 생성 로직 추가

--- models/MySQL.sql
+++ models/MySQL.sql
@@ -6,3 +6,13 @@ CREATE DATABASE my_team;
 -- 데이터베이스 선택
 USE DATABASE my_team; -- 틀린 작성법
 USE my_team; -- 옳은 작성법
+
+-- 테이블 생성
+CREATE TABLE info (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  personal_site VARCHAR(255),
+  contact VARCHAR(255),
+  current_project VARCHAR(255),
+  img VARCHAR(255)
+);
