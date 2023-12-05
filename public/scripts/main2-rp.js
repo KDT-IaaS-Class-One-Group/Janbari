e2cd0cff28c666ebb054c8af267d66338bf376c1
Yusmin97 <tmdamsi97@gmail.com>
2023-12-05 12:26:08 +0900

    ♻️ Refactor: 코드 모듈화
    setProfileStyle 함수 모듈화

--- public/scripts/main2-rp.js
+++ public/scripts/main2-rp.js
@@ -5,14 +5,7 @@ const lee = document.getElementById('lee');
 const container = document.getElementById('container');
 const initialHTML = container.innerHTML;
 let jsonData; // JSON 데이터를 저장할 변수
-
-// json으로 가져올 이미지 값을 함수로 미리 지정
-function setProfileStyle(element, imgUrl) {
-  element.style.backgroundImage = `url(${imgUrl})`;
-  element.style.backgroundSize = 'cover';
-  element.style.borderRadius = '20%';
-  element.style.cursor = 'pointer';
-}
+import { setProfileStyle } from "./profileStyle.js";
 
 // 프로필 이미지 클릭 이벤트를 함수로 생성
 function profileClick(element, profileId) {
