2b8e37fb4b905648a3f54df3a7112757e9fcb391
Yusmin97 <tmdamsi97@gmail.com>
2023-12-05 14:11:07 +0900

    ✏️ Modify: 모듈화 실패(삭제)
    모듈화로 인해 메인 js의 기능이 불통

--- public/scripts/profileStyle.js
+++
@@ -1,7 +0,0 @@
-// json으로 가져올 이미지 값을 함수로 미리 지정
-export function setProfileStyle(element, imgUrl) {
-  element.style.backgroundImage = `url(${imgUrl})`;
-  element.style.backgroundSize = 'cover';
-  element.style.borderRadius = '20%';
-  element.style.cursor = 'pointer';
-}
