9d44fc32d797ca9d32ae378f6fad535812618391
eunjeong <eunjeong990731@gmail.com>
2023-12-05 14:52:45 +0900

    :recycle: Refactor : 중복 줄이기
    중복된 이름과 json 객체로 변환

--- public/scripts/main2.js
+++ public/scripts/main2.js
@@ -1,7 +1,10 @@
-const ho = document.getElementById('ho');
-const yu = document.getElementById('yu');
-const so = document.getElementById('so');
-const lee = document.getElementById('lee');
+const profileElements = [
+  { id: 'ho', jsonKey: '잔잔바리/호녕' },
+  { id: 'yu', jsonKey: '잔잔바리/승민' },
+  { id: 'so', jsonKey: '잔잔바리/사무엘' },
+  { id: 'lee', jsonKey: '잔잔바리/은정' },
+];
+
 const container = document.getElementById('container');
 const initialHTML = container.innerHTML;
 let jsonData; // JSON 데이터를 저장할 변수
@@ -11,60 +14,64 @@ fetch('/json')
   .then((data) => {
     jsonData = data; // JSON 데이터 저장
 
-    ho.style.backgroundImage = `url(${data['잔잔바리/호녕'].img})`;
-    yu.style.backgroundImage = `url(${data['잔잔바리/승민'].img})`;
-    so.style.backgroundImage = `url(${data['잔잔바리/사무엘'].img})`;
-    lee.style.backgroundImage = `url(${data['잔잔바리/은정'].img})`;
-
-    const name = [ho, yu, so, lee];
-
-    for (let i = 0; i < name.length; i++) {
-        name[i].style.backgroundSize = 'cover';
-        name[i].style.borderRadius = '20%';
-        name[i].style.cursor = 'pointer';
-    }
-
-    ho.addEventListener('click', () => handleProfileClick('잔잔바리/호녕'));
-    yu.addEventListener('click', () => handleProfileClick('잔잔바리/승민'));
-    so.addEventListener('click', () => handleProfileClick('잔잔바리/사무엘'));
-    lee.addEventListener('click', () => handleProfileClick('잔잔바리/은정'));
+    // 프로필 이미지 및 스타일 설정
+    profileElements.forEach((profile) => {
+      const element = document.getElementById(profile.id);
+      setProfileStyle(element, data[profile.jsonKey].img);
+      profileClick(element, profile.jsonKey);
+    });
   })
   .catch((error) => console.error('Error fetching JSON', error));
 
-function handleProfileClick(profileId) {
+function setProfileStyle(element, imgUrl) {
+  element.style.backgroundImage = `url(${imgUrl})`;
+  element.style.backgroundSize = 'cover';
+  element.style.borderRadius = '20%';
+  element.style.cursor = 'pointer';
+}
+
+function profileClick(element, profileKey) {
+  element.addEventListener('click', () => handleProfileClick(profileKey));
+}
+
+function handleProfileClick(profileKey) {
   // 기존 내용 지우기
   container.innerHTML = '';
 
   if (jsonData) {
-    const profileData = jsonData[profileId];
+    const profileData = jsonData[profileKey];
 
     // 새로운 div에 데이터 추가
     const newDiv = document.createElement('div');
     newDiv.classList.add('new-profile');
-    newDiv.innerHTML = `
-      <div class="position-abs left-88vw top-33vh">
-        <button class="fontSiez-2rem bgc-black border-none cursor-pointer" onclick="handleBack()">❌</button>
-      </div>
-      <div class="display-flex flex-column width-42-5vw height-65vh">
-        <div class="display-flex justify-center align-center width-42-5vw height-10vh">
-          <h2 class="fontSiez-3rem margin-top-1vh">${profileData.name}</h2>
-        </div>  
-        <div class="display-flex justify-center align-center width-42-5vw height-55vh">  
-          <img class="width-20vw height-20vw border-orange" src="${profileData.img}" alt="Profile Image">
-        </div>
-      </div>
-      <div class="display-flex flex-column justify-center width-42-5vw height-65vh lineHeight">
-        <p class="pSize"><b>GitHub: </b>${profileData.personal_site}</p>
-        <p class="pSize"><b>E-mail: </b>${profileData.contact}</p>
-        <p class="pSize"><b>Projects: </b> <a href="${profileData.current_project}" target="_blank">${profileData.current_project}</a></p>
-      </div>  
-    `;
+    newDiv.innerHTML = createProfileHTML(profileData);
 
     // 새로운 div를 container에 추가
     container.appendChild(newDiv);
   } else {
     console.error('JSON data is not available.');
   }
+}
+
+function createProfileHTML(profileData) {
+  return `
+    <div class="position-abs left-88vw top-33vh">
+      <button class="fontSiez-2rem bgc-black border-none cursor-pointer" onclick="handleBack()">❌</button>
+    </div>
+    <div class="display-flex flex-column width-42-5vw height-65vh">
+      <div class="display-flex justify-center align-center width-42-5vw height-10vh">
+        <h2 class="fontSiez-3rem margin-top-1vh">${profileData.name}</h2>
+      </div>  
+      <div class="display-flex justify-center align-center width-42-5vw height-55vh">  
+        <img class="width-20vw height-20vw" src="${profileData.img}" alt="Profile Image">
+      </div>
+    </div>
+    <div class="display-flex flex-column justify-center width-42-5vw height-65vh lineHeight">
+      <p class="pSize"><b>GitHub: </b>${profileData.personal_site}</p>
+      <p class="pSize"><b>E-mail: </b>${profileData.contact}</p>
+      <p class="pSize"><b>Projects: </b> <a href="${profileData.current_project}" target="_blank">${profileData.current_project}</a></p>
+    </div>  
+  `;
 }
 
 function handleBack() {
