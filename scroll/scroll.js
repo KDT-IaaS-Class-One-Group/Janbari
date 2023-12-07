6ee57d2221f1935ec17d1537c82275869626d382
Ho Nyeong <dev.honing@gmail.com>
2023-12-07 11:57:50 +0900

    :pencil2: Modify: JSDoc 주석 추가

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -1,9 +1,15 @@
 // scroll.js
 
+/**
+ * 페이지가 로드되면 이벤트를 실행하는 함수
+ * @function
+ * @description 3초 후에 header의 높이를 10vh로 변경한다.
+ */
 document.addEventListener('DOMContentLoaded', function () {
-  // 페이지 로드 후 3초 뒤에 실행되는 애니메이션
+  // 3초 뒤 실행하는 메서드
   setTimeout(() => {
     var header = document.getElementById('header');
+    // header의 height를 10vh로 변경
     header.style.height = "10vh";
   }, 3000);
 });
