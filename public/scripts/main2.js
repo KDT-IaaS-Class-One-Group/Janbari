174b67e37f58d29554f9c94f878d3ae1574f1d9f
eunjeong <eunjeong990731@gmail.com>
2023-12-07 10:53:27 +0900

    :art: Design : 정렬을 세로로 변경
    정렬을 세로로 변경하고 클릭이벤트를 삭제
    스크롤 이벤트를 통해서 home 부분을 내리면 프로필이 하나씩 뜨도록 설정, info json 잘 가져오도록 하기

--- public/scripts/main2.js
+++ public/scripts/main2.js
@@ -1,4 +1,4 @@
-//* 프로필 정보를 담은 배열,객체 html의 id와 json데이터의 키로 구성되어있다.
+// main2.js
 const profileElements = [
   { id: 'ho', jsonKey: '잔잔바리/호녕' },
   { id: 'yu', jsonKey: '잔잔바리/승민' },
@@ -7,10 +7,8 @@ const profileElements = [
 ];
 
 const container = document.getElementById('container');
-const initialHTML = container.innerHTML;
-let jsonData; // JSON 데이터를 저장할 변수
+let jsonData;
 
-//* async는 json 데이터를 가져오는 비동기 함수 fatch를 사용해서 서버에서 json 데이터를 받아온다.
 async function fetchData() {
   try {
     const response = await fetch('/json');
@@ -19,94 +17,62 @@ async function fetchData() {
     console.error('Error fetching JSON', error);
   }
 }
-//* 프로필을 가져오는 함수
+
 function renderProfiles() {
-  //* 배열을 순회하면서 스타일을 지정하고 클릭이벤트를 추가한다.
   profileElements.forEach(({ id, jsonKey }) => {
-    //* 현재 순회중인 프로필요소의 html요소를 id를 사용하여 가져온다.
     const element = document.getElementById(id);
-    //*프로필 스타일을 설정한다.
     setProfileStyle(element, jsonData[jsonKey].img);
-    //* 클릭이벤트를 생성한다.
-    profileClick(element, jsonKey);
   });
 }
-//* 프로필 이미지의 스타일 지정
+
 function setProfileStyle(element, imgUrl) {
   element.style.backgroundImage = `url(${imgUrl})`;
   element.style.backgroundSize = 'cover';
   element.style.borderRadius = '20%';
   element.style.cursor = 'pointer';
 }
-//* 클릭이벤트 설정
-function profileClick(element, profileKey) {
-  //* 클릭을 하면 handleProfileClick 함수를 호출하게 된다.
-  //* 컨테이너 innerHtml 빈칸
-  element.addEventListener('click', () => handleProfileClick(profileKey));
+
+function addHomeButtonClickEvent() {
+  const homeButton = document.getElementById('homeButton');
+  homeButton.addEventListener('click', scrollToTHome);
 }
 
-function handleProfileClick(profileKey) {
-  // 기존 내용 지우기
-  container.innerHTML = '';
+function addProfileButtonClickEvent() {
+  const profileButton = document.getElementById('profileButton');
+  profileButton.addEventListener('click', scrollToTProfile);
+}
 
-  if (jsonData) {
-    const profileData = jsonData[profileKey];
+function scrollToTHome() {
+  document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+}
 
-    // 새로운 div에 데이터 추가
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('new-profile');
-    newDiv.innerHTML = createProfileHTML(profileData);
+function scrollToTProfile() {
+  container.scrollIntoView({ behavior: 'smooth' });
+}
 
-    // 새로운 div를 container에 추가
-    container.appendChild(newDiv);
-  } else {
-    console.error('JSON data is not available.');
+let currentProfileIndex = 0;
+let profilesRendered = false;
+window.addEventListener('scroll', handleScroll);
+
+async function handleScroll() {
+  const bottomOfScreen = window.innerHeight + window.scrollY;
+
+  if (!profilesRendered) {
+    const nextProfileElement = document.getElementById(profileElements[currentProfileIndex].id);
+    const profileBottom = nextProfileElement.offsetTop + nextProfileElement.offsetHeight;
+
+    if (bottomOfScreen > profileBottom) {
+      await fetchData();
+      renderProfiles();
+      currentProfileIndex += 1;
+
+      if (currentProfileIndex === profileElements.length) {
+        profilesRendered = true;
+      }
+    }
   }
 }
 
-function createProfileHTML(profileData) {
-  return `
-    <div class="position-abs left-88vw top-33vh">
-      <button class="fontSiez-2rem bgc-black border-none cursor-pointer" onclick="handleBack()">❌</button>
-    </div>
-    <div class="display-flex flex-column width-42-5vw height-65vh">
-      <div class="display-flex justify-center align-center width-42-5vw height-10vh">
-        <h2 class="fontSiez-3rem margin-top-1vh">${profileData.name}</h2>
-      </div>  
-      <div class="display-flex justify-center align-center width-42-5vw height-55vh">  
-        <img class="width-20vw height-20vw" src="${profileData.img}" alt="Profile Image">
-      </div>
-    </div>
-    <div class="display-flex flex-column justify-center width-42-5vw height-65vh lineHeight">
-      <p class="pSize"><b>GitHub: </b>${profileData.personal_site}</p>
-      <p class="pSize"><b>E-mail: </b>${profileData.contact}</p>
-      <p class="pSize"><b>Projects: </b> <a href="${profileData.current_project}" target="_blank">${profileData.current_project}</a></p>
-    </div>  
-  `;
-}
-
-function handleBack() {
-  // 초기 화면으로 돌아가는 코드 추가
-  // 예: location.reload(); 또는 contentContainer.innerHTML = '초기화면의 HTML 코드';
-  location.reload(); // 페이지 새로고침을 통해 초기 상태로 돌아가는 예시
-}
-
-// fetchData 함수 호출 및 데이터 렌더링
+addHomeButtonClickEvent();
+addProfileButtonClickEvent();
 fetchData().then(renderProfiles);
-
-
-// header home, profile 변수
-const homeButton = document.getElementById('homeButton')
-const profileButton = document.getElementById('profileButton')
-
-// 클릭하여 스크롤로 이동하는 함수
-// * 리팩토링 필요
-function scrollToTHome() {
-  home.scrollIntoView({ behavior: 'smooth'});
-}
-function scrollToTProfile() {
-  container.scrollIntoView({ behavior: 'smooth'});
-}
-
-homeButton.addEventListener('click', scrollToTHome)
-profileButton.addEventListener('click', scrollToTProfile)
